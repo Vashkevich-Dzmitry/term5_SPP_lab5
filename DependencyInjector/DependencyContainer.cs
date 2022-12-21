e0e31401bbfd962fdb38027bd76532810a403ef4
dzmitry <dzmitry.vashkevich.a@gmail.com>
2022-12-21 18:36:06 +0300

    add dependency container and interface

---
+++ DependencyInjector/DependencyContainer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjector
+{
+    public class DependencyContainer : IDependencyContainer
+    {
+        public Dictionary<Type, List<Implementation>> ImplementationsDictionary { get; set; }
+        public Stack<Type> RecursionStack { get; set; }
+
+        public DependencyContainer()
+        {
+            ImplementationsDictionary = new Dictionary<Type, List<Implementation>>();
+            RecursionStack = new Stack<Type>();
+        }
+
+        public TDependency Resolve<TDependency>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register<TDependency, TImplementation>(bool isSingleton)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DependencyInjector/IDependencyContainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjector
+{
+    public interface IDependencyContainer
+    {
+        public Dictionary<Type, List<Implementation>> ImplementationsDictionary { get; set; }
+        public Stack<Type> RecursionStack { get; set; }
+
+        public TDependency Resolve<TDependency>();
+        public void Register<TDependency, TImplementation>(bool isSingleton);
+    }
+}
