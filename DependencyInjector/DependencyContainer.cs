7a923f2b73258f93e546a992471b26bea001cac7
dzmitry <dzmitry.vashkevich.a@gmail.com>
2022-12-21 18:50:30 +0300

    final without tests

--- DependencyInjector/DependencyContainer.cs
+++ DependencyInjector/DependencyContainer.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DependencyInjector
+﻿namespace DependencyInjector
 {
     public class DependencyContainer : IDependencyContainer
     {
@@ -65,25 +59,123 @@ namespace DependencyInjector
             if (tDependency.IsGenericType &&
                 tDependency.GetGenericTypeDefinition() == typeof(IEnumerable<>))
             {
-
+                Type argumentType = tDependency.GetGenericArguments()[0];
+                var implementations = new List<Implementation>(GetImplementationsForDependency(argumentType));
+                var createdArguments = (object[])Activator.CreateInstance(argumentType.MakeArrayType(), new object[] { implementations.Count });
+                for (var i = 0; i < implementations.Count; i++)
+                {
+                    createdArguments[i] = CreateObjectOrGetObjectIfSingleton(implementations[i]);
+                }
+                result = createdArguments;
             }
             else
             {
+                var implementations = new List<Implementation>();
                 if (tDependency.IsGenericType &&
                     ImplementationsDictionary.ContainsKey(tDependency.GetGenericTypeDefinition()))
                 {
-                    
+                    implementations = new List<Implementation>(GetImplementationsForDependency(tDependency.GetGenericTypeDefinition()));
                 }
                 else
                 {
                     if (ImplementationsDictionary.ContainsKey(tDependency))
                     {
-                        
+                        implementations = new List<Implementation>(GetImplementationsForDependency(tDependency));
                     }
                 }
-                
+                if (implementations.Any())
+                {
+                    result = CreateObjectOrGetObjectIfSingleton(implementations[0]);
+                }
+                else
+                {
+                    result = CreateUsingConstructor(tDependency);
+                }
             }
             RecursionStack.Pop();
+            return result;
+        }
+
+        public List<Implementation>? GetImplementationsForDependency(Type tDependency)
+        {
+            if (ImplementationsDictionary.ContainsKey(tDependency))
+            {
+                return ImplementationsDictionary[tDependency];
+            }
+            return null;
+        }
+
+        private object CreateObjectOrGetObjectIfSingleton(Implementation implementation)
+        {
+            if (implementation.IsSingleton)
+            {
+                if (implementation.SingletonObject == null)
+                {
+                    lock (implementation)
+                    {
+                        if (implementation.SingletonObject == null)
+                        {
+                            implementation.SingletonObject = Resolve(implementation.Type);
+                            return implementation.SingletonObject;
+                        }
+                    }
+                }
+                return implementation.SingletonObject;
+            }
+            else
+            {
+                return Resolve(implementation.Type);
+            }
+        }
+        private object CreateUsingConstructor(Type type)
+        {
+            object? result = null;
+
+            if (type.IsGenericType)
+            {
+                var genericArguments = type.GetGenericArguments();
+                var genericParams = genericArguments.Select(dependency =>
+                {
+                    var implementations = DConfig
+                        .GetImplementationsForDependency(dependency.BaseType!)?.ToArray();
+                    if (implementations == null)
+                    {
+                        return dependency.BaseType;
+                    }
+                    else
+                    {
+                        return implementations.First().Type;
+                    }
+                }).ToArray();
+
+                type = type.MakeGenericType(genericParams);
+            }
+
+            var constructorsInfo = type.GetConstructors();
+            foreach (var constructorInfo in constructorsInfo)
+            {
+                var parameters = new List<object>();
+
+                try
+                {
+                    var paramsInfo = constructorInfo.GetParameters();
+                    foreach (var paramInfo in paramsInfo)
+                    {
+                        parameters.Add(Resolve(paramInfo.ParameterType));
+                    }
+
+                    result = Activator.CreateInstance(type, parameters.ToArray());
+                    if (result != null)
+                    {
+                        return result;
+                    }
+                }
+                catch (StackOverflowException e)
+                {
+                    throw e;
+                }
+                catch { }
+            }
             return result;
         }
     }
--- DependencyInjector/IDependencyContainer.cs
+++ DependencyInjector/IDependencyContainer.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DependencyInjector
+﻿namespace DependencyInjector
 {
     public interface IDependencyContainer
     {
