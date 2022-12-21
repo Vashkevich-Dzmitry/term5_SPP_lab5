0c4fe28161681046cbf189e957c19fa37d5d7a28
dzmitry <dzmitry.vashkevich.a@gmail.com>
2022-12-21 18:16:44 +0300

    Implementation

---
+++ DependencyInjector/Implementation.cs
@@ -0,0 +1,15 @@
+﻿namespace DependencyInjector
+{
+    public class Implementation
+    {
+        public Type Type { get; set; }
+        public bool IsSingleton { get; set; }
+        public object? SingletonObject { get; set; }
+        public Implementation(Type implementation, bool isSingleton)
+        {
+            Type = implementation;
+            IsSingleton = isSingleton;
+            SingletonObject = null;
+        }
+    }
+}
